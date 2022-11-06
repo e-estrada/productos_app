6d6da470657acdd9b67c6b73bb43fc7bd0a92670
Erick Estrada <eof105@gmail.com>
2022-11-06 13:26:24 -0600

    Formulario de Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,12 +21,54 @@ class LoginScreen extends StatelessWidget {
                     Text('Login', style: Theme.of(context).textTheme.headline4,),
                     const SizedBox(height: 30),
 
-                    
+                    _LoginForm()
+
                   ],
                 ),
-              )
+              ),
+
+              const SizedBox(height: 50),
+              const Text('Crear una nueva cuenta.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+
             ],
           )
+        )
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.purple
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                hintText: 'correo@example.com',
+                labelText: 'Correo electrónico',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple,)
+              ),
+            )
+          ],
         )
       ),
     );
