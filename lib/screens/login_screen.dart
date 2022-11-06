9d7bb34d3f965c874e44b9157eb6e837cba1ace5
Erick Estrada <eof105@gmail.com>
2022-11-06 14:28:59 -0600

    Validaciones del formulario

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,39 +3,34 @@ import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
-   
   const LoginScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 250 ),
-
-              CardContainer(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    Text('Login', style: Theme.of(context).textTheme.headline4,),
-                    const SizedBox(height: 30),
-
-                    _LoginForm()
-
-                  ],
+          child: SingleChildScrollView(
+              child: Column(
+        children: [
+          const SizedBox(height: 250),
+          CardContainer(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
-
-              const SizedBox(height: 50),
-              const Text('Crear una nueva cuenta.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 50),
-
-            ],
-          )
-        )
-      ),
+                const SizedBox(height: 30),
+                _LoginForm()
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          const Text('Crear una nueva cuenta.', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 50),
+        ],
+      ))),
     );
   }
 }
@@ -45,44 +40,55 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Form(
-        child: Column(
-          children: [
-            TextFormField(
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecorations.authInputDecoration(hintText: 'email@example.com', labelText: 'Correo electrónico', prefixIcon: Icons.alternate_email_sharp),
-            ),
-            
-            const SizedBox(height: 30),
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            children: [
 
-            TextFormField(
-              autocorrect: false,
-              obscureText: true,
-              decoration: InputDecorations.authInputDecoration(hintText: '****', labelText: 'Contraseña', prefixIcon: Icons.lock_clock_outlined),
-            ),
-
-            const SizedBox(height: 30),
-
-            MaterialButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'email@example.com', labelText: 'Correo electrónico', prefixIcon: Icons.alternate_email_sharp),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = RegExp(pattern);
+                  return regExp.hasMatch(value ?? '') ? null : 'El correo no es correcto.';
+                },
               ),
-              disabledColor: Colors.grey,
-              elevation: 0,
-              color: Colors.deepPurple,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: const Text('Ingresar', style: TextStyle(color: Colors.white)),
+
+              const SizedBox(height: 30),
+
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                decoration: InputDecorations.authInputDecoration(hintText: '****', labelText: 'Contraseña', prefixIcon: Icons.lock_clock_outlined),
+                validator: (value) {
+                  return (value != null && value.length >= 6) 
+                  ? null
+                  : 'La contraseña debe de ser de 6 caracteres.';
+                },
               ),
-              onPressed: () {
+
+              const SizedBox(height: 30),
+
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: const Text('Ingresar', style: TextStyle(color: Colors.white)),
+                ),
+                onPressed: () {},
+              ),
+
               
-              },
-            )
-
-
-          ],
-        )
-      ),
+            ],
+          )),
     );
   }
 }
