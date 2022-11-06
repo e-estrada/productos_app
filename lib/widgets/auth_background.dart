2f806fa028fc488706eccb5000c7f511c679a34b
Erick Estrada <eof105@gmail.com>
2022-11-06 12:48:39 -0600

    Icono Central y widgets como argumentos

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
 
 class AuthBackground extends StatelessWidget {
-  const AuthBackground({super.key});
-
+  const AuthBackground({super.key, required this.child});
+  final Widget child;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       height: double.infinity,
-      child: Stack(
-        children: [
-          _PurpleBox()
-        ]
+      child: Stack(children: [
+        _PurpleBox(),
+        _HeaderIcon(),
+
+        child,
+      ]),
+    );
+  }
+}
+
+class _HeaderIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 30),
+        child: const Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 100,
+        ),
       ),
     );
   }
@@ -37,14 +55,8 @@ class _PurpleBox extends StatelessWidget {
     );
   }
 
-  BoxDecoration _purpleBackground() => const BoxDecoration(
-    gradient: LinearGradient(
-      colors: [
-        Color.fromRGBO(63, 63, 156, 1),
-        Color.fromRGBO(90, 70, 178, 1)
-      ]
-    )
-  );
+  BoxDecoration _purpleBackground() =>
+      const BoxDecoration(gradient: LinearGradient(colors: [Color.fromRGBO(63, 63, 156, 1), Color.fromRGBO(90, 70, 178, 1)]));
 }
 
 class _Bubble extends StatelessWidget {
@@ -53,10 +65,7 @@ class _Bubble extends StatelessWidget {
     return Container(
       width: 100,
       height: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        color: const Color.fromRGBO(255, 255, 255, 0.05)
-      ),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: const Color.fromRGBO(255, 255, 255, 0.05)),
     );
   }
 }
