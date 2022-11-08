365383c711722963c51b5bd66994f47667c42cc8
Erick Estrada <eof105@gmail.com>
2022-11-08 15:48:14 -0600

    Cargar datos del producto en el formulario

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/product_form_provider.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
-   
   const ProductScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     final productService = Provider.of<ProductsServices>(context);
+    return ChangeNotifierProvider(
+      create: (_) => ProductFormProvider(productService.selectedProduct),
+      child: _ProductsScreenBody(productService: productService),
+    );
+  }
+}
+
+class _ProductsScreenBody extends StatelessWidget {
+  const _ProductsScreenBody({
+    Key? key,
+    required this.productService,
+  }) : super(key: key);
+
+  final ProductsServices productService;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -19,40 +36,24 @@ class ProductScreen extends StatelessWidget {
               children: [
                 ProductImage(url: productService.selectedProduct.picture),
                 Positioned(
-                  top: 60,
-                  left: 20,
-                  child:  IconButton(
-                    onPressed: () => Navigator.of(context).pop(), 
-                    icon: const Icon(Icons.arrow_back_ios_new, size: 40, color: Colors.white)
-                  )
-                ),
-
+                    top: 60,
+                    left: 20,
+                    child: IconButton(
+                        onPressed: () => Navigator.of(context).pop(), icon: const Icon(Icons.arrow_back_ios_new, size: 40, color: Colors.white))),
                 Positioned(
                     top: 60,
                     right: 20,
-                    child: IconButton(
-                        onPressed: () => {
-                          
-                        }, 
-                        icon: const Icon(Icons.camera_alt_outlined, size: 40, color: Colors.white)
-                    )
-                )
-
-
+                    child: IconButton(onPressed: () => {}, icon: const Icon(Icons.camera_alt_outlined, size: 40, color: Colors.white)))
               ],
             ),
-
             _ProductoForm(),
-
             const SizedBox(height: 100)
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-        },
+        onPressed: () {},
         child: const Icon(Icons.save_outlined),
       ),
     );
@@ -62,65 +63,58 @@ class ProductScreen extends StatelessWidget {
 class _ProductoForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productForm = Provider.of<ProductFormProvider>(context);
+    final product = productForm.product;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        width: double.infinity,        
+        width: double.infinity,
         decoration: _builsBoxDecoration(),
         child: Form(
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-
-              TextFormField(
-                decoration: InputDecorations.authInputDecoration(
-                  hintText: 'Nombre del producto', 
-                  labelText: 'Nombre:'
-                ),
-              ),
-
-
-              const SizedBox(height: 30),
-
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecorations.authInputDecoration(
-                  hintText: '\$150', 
-                  labelText: 'Precio:'
-                ),
-              ),
-
-              const SizedBox(height: 30),
-
-              SwitchListTile.adaptive(
-                title: const Text('Disponible'),
-                activeColor: Colors.indigo,
-                value: true, 
-                onChanged: (value) {
-                  
-                },
-              ),
-
-
-              const SizedBox(height: 30),
-
-            ],
-          )
-        ),
+            child: Column(
+          children: [
+            const SizedBox(height: 10),
+            TextFormField(
+              initialValue: product.name,
+              onChanged: (value) => product.name = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'El nombre es requerido';
+                }
+                return null;
+              },
+              decoration: InputDecorations.authInputDecoration(hintText: 'Nombre del producto', labelText: 'Nombre:'),
+            ),
+            const SizedBox(height: 30),
+            TextFormField(
+              initialValue: '${product.price}',
+              onChanged: (value) {
+                if (double.tryParse(value) == null) {
+                  product.price = 0;
+                } else {
+                  product.price = double.parse(value);
+                }
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInputDecoration(hintText: '\$150', labelText: 'Precio:'),
+            ),
+            const SizedBox(height: 30),
+            SwitchListTile.adaptive(
+              title: const Text('Disponible'),
+              activeColor: Colors.indigo,
+              value: product.available,
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 30),
+          ],
+        )),
       ),
     );
   }
 
   BoxDecoration _builsBoxDecoration() => BoxDecoration(
-    color: Colors.white,
-    borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
-    boxShadow: [
-      BoxShadow(
-        color: Colors.black.withOpacity(0.05),
-        offset: const Offset(0,5),
-        blurRadius: 5
-      )
-    ]
-  );
+      color: Colors.white,
+      borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+      boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.05), offset: const Offset(0, 5), blurRadius: 5)]);
 }
